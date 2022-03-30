08946b457dc3d952e7ebf013e7314cb00e32797f
Eyal <eyal.davidi@edu.turkuamk.fi>
2022-03-30 10:44:36 +0200

    Creating the layout for fragment_game_play + handling the navigation

--- app/src/main/java/com/example/madlevel4task2/GameHistoryFragment.kt
+++ app/src/main/java/com/example/madlevel4task2/GameHistoryFragment.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.madlevel4task2.databinding.FragmentFirstBinding
+import com.example.madlevel4task2.databinding.FragmentGameHistoryBinding
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class GameHistoryFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentGameHistoryBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +24,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentGameHistoryBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,9 +32,6 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
     }
 
     override fun onDestroyView() {

cc0cc9744157f5c9c2ebf360c463eaa5fa11c047
Eyal <eyal.davidi@edu.turkuamk.fi>
2022-03-31 14:26:08 +0200

    Setting up the bottom navigation bar

--- app/src/main/java/com/example/madlevel4task2/Game.kt
+++ app/src/main/java/com/example/madlevel4task2/Game.kt
@@ -3,10 +3,7 @@ package com.example.madlevel4task2
 import java.time.LocalDateTime
 
 data class Game (
-    ///game date type?
-
-
-    val gameDate: LocalDateTime,
+    var gameDate: LocalDateTime,
     var youMove: GameMove,
     var computerMove: GameMove,
     var gameResult: String = "",

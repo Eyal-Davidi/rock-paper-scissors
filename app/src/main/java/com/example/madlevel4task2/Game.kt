3463b4b80df3ccbea2a9becd85b5ba3363fe9350
Eyal <eyal.davidi@edu.turkuamk.fi>
2022-04-02 13:11:50 +0200

    Revert "Setting up the bottom navigation bar"
    This reverts commit cc0cc974

--- app/src/main/java/com/example/madlevel4task2/Game.kt
+++ app/src/main/java/com/example/madlevel4task2/Game.kt
@@ -3,7 +3,10 @@ package com.example.madlevel4task2
 import java.time.LocalDateTime
 
 data class Game (
-    var gameDate: LocalDateTime,
+    ///game date type?
+
+
+    val gameDate: LocalDateTime,
     var youMove: GameMove,
     var computerMove: GameMove,
     var gameResult: String = "",
--- app/src/main/java/com/example/madlevel4task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel4task2/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.madlevel4task2
 
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
+import android.view.Menu
+import android.view.MenuItem
 import com.example.madlevel4task2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,14 +24,8 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment)
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
-
-        // Since we specified in our bottom_navigation_menu.xml file where to navigate to(id), we need no more navigation logic!
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-
-        binding.includeMain.bottomNavigation.setupWithNavController(navHostFragment.navController)
         }
     }

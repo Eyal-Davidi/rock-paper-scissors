e8fa7b76af012cf4ab40540a7cc4e97ba600dd87
Eyal <eyal.davidi@edu.turkuamk.fi>
2022-04-06 16:11:42 +0200

    Setting a type convertor to LocalDateTime

---
+++ app/src/main/java/com/example/madlevel4task2/Converters.kt
@@ -0,0 +1,32 @@
+package com.example.madlevel4task2
+
+import androidx.room.TypeConverter
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+
+class Converters {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): LocalDateTime? {
+        return value?.let { LocalDateTime.ofInstant(Instant.ofEpochMilli(value), ZoneOffset.UTC) }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: LocalDateTime?): Long? {
+        return date?.atZone(ZoneOffset.UTC)?.toInstant()?.toEpochMilli()
+    }
+}
+
+/*
+class Converters {
+  @TypeConverter
+  fun fromTimestamp(value: Long?): Date? {
+    return value?.let { Date(it) }
+  }
+
+  @TypeConverter
+  fun dateToTimestamp(date: Date?): Long? {
+    return date?.time?.toLong()
+  }
+}
+ */
--- app/src/main/java/com/example/madlevel4task2/GameRoomDatabase.kt
+++ app/src/main/java/com/example/madlevel4task2/GameRoomDatabase.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(entities = [Game::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class GameRoomDatabase : RoomDatabase() {
 
     abstract fun gameDao(): GameDao
